17accfec337ac4b81e7417f40f21aea014a0a763
Maia Dutta <mermaia33@gmail.com>
2023-01-07 13:19:50 -0600

    added get single resource function to all request modules and code to create new orders

--- views/metals_requests.py
+++ views/metals_requests.py
@@ -30,3 +30,18 @@ METALS = [
 def get_all_metals():
     """returns METALS list of dictionaries"""
     return METALS
+# Function with a single parameter
+
+def get_single_metal(id):
+    """Variable to hold the found metal, if it exists"""
+    requested_metal = None
+
+    # Iterate the METALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for metal in METALS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if metal["id"] == id:
+            requested_metal = metal
+
+    return requested_metal
